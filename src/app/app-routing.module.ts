c27909d82dc90a68d6385b935d75d17553aaf40b
Tim <timdekievit@hotmail.com>
2023-08-07 13:43:21 +0200

    first homepage setup + added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'products', component: ProductsComponent}
+];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
