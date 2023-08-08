011c808d2bbd40ffae7e805209fcc87007b7b3a1
Tim <timdekievit@hotmail.com>
2023-08-08 14:06:42 +0200

    basic shopping-cart working

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Item } from '../models/item';
 
 @Component({
@@ -6,11 +6,17 @@ import { Item } from '../models/item';
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.scss']
 })
-export class ShoppingCartComponent {
+export class ShoppingCartComponent implements OnInit {
+  dummyItem: Item = new Item('1', 'Dummy Item', 100, 1, false, '../../assets/images/shoe.png');
 
   // eventually the items need to be fetched from the server
   shoppingCartItems: Item[] = [];
-
+  
+  
+  ngOnInit(): void {
+    this.shoppingCartItems.push(this.dummyItem);
+    
+  }
 
   removeItemFromCart(item: Item) {
       this.shoppingCartItems = this.shoppingCartItems.filter((i) => i.id !== item.id);
