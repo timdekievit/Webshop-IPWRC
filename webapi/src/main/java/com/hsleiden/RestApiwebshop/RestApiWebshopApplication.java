59fb13c912129002321a624b8deddde79f837989
Tim <timdekievit@hotmail.com>
2023-11-16 15:50:17 +0100

    start java spring boot webapi

---
+++ webapi/src/main/java/com/hsleiden/RestApiwebshop/RestApiWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.hsleiden.RestApiwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWebshopApplication.class, args);
+	}
+
+}
---
+++ webapi/src/test/java/com/hsleiden/RestApiwebshop/RestApiWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsleiden.RestApiwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
