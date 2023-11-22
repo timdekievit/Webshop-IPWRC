462b4c6c0d21ca3a80662589632a7d565de5cd8b
Tim <timdekievit@hotmail.com>
2023-11-22 16:52:55 +0100

    TODO toegevoegd en verder werken aan de image upload

--- Webapi/src/main/java/com/hsleiden/Webapi/controller/ImageController.java
+++ Webapi/src/main/java/com/hsleiden/Webapi/controller/ImageController.java
@@ -1,13 +1,12 @@
 package com.hsleiden.Webapi.controller;
 
 import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
+import com.hsleiden.Webapi.service.CloudinaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/images")
@@ -15,6 +14,8 @@ public class ImageController {
 
     @Autowired
     private Cloudinary cloudinary;
+    @Autowired
+    private CloudinaryService cloudinaryService;
 
     @PostMapping("/upload")
     public String uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
@@ -22,18 +23,20 @@ public class ImageController {
             return "Please select a file to upload.";
         }
 
+
+
         try {
             // Convert MultipartFile to byte array
             byte[] imageData = file.getBytes();
 
+            // TODO fix de naam van de image die wordt opgestuurd.
             // Generate a unique filename (you might want to implement a more robust strategy)
-            String filename = "image_" + System.currentTimeMillis();
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String filename = originalFilename + System.currentTimeMillis() + extension;
 
             // Upload the image to Cloudinary
-            Map<?, ?> uploadResult = cloudinary.uploader().upload(imageData, ObjectUtils.asMap("public_id", filename));
-
-            // Retrieve the public URL of the uploaded image
-            String imageUrl = uploadResult.get("url").toString();
+            String imageUrl = cloudinaryService.uploadImage(imageData, filename);
 
             return "Image uploaded successfully. Public URL: " + imageUrl;
         } catch (IOException e) {
