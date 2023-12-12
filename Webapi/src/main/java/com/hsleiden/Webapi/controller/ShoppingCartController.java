4deac46dc34c7bcf9ab88e05f833558731e3169e
Tim <timdekievit@hotmail.com>
2023-12-12 14:31:30 +0100

    refactored ShoppingCartController

--- Webapi/src/main/java/com/hsleiden/Webapi/controller/ShoppingCartController.java
+++ Webapi/src/main/java/com/hsleiden/Webapi/controller/ShoppingCartController.java
@@ -22,83 +22,18 @@ public class ShoppingCartController {
 
     private final ShoppingCartService shoppingCartService;
 
-    private final UserRepository userRepository;
-    private final ShoppingCartRepository shoppingCartRepository;
-    private final ProductRepository productRepository;
-
     @Autowired
-    public ShoppingCartController(ShoppingCartService shoppingCartService, UserRepository userRepository, ShoppingCartRepository shoppingCartRepository, ProductRepository productRepository) {
+    public ShoppingCartController(ShoppingCartService shoppingCartService) {
         this.shoppingCartService = shoppingCartService;
-        this.userRepository = userRepository;
-        this.productRepository = productRepository;
-        this.shoppingCartRepository = shoppingCartRepository;
     }
 
-
-    // TODO get the shopping cart with email from the user, then get the correct shopping cart with that email and return it
     @GetMapping()
     public ResponseEntity<List<Product>> getShoppingCartInfo() {
-        // Retrieve the currently authenticated user's username from the security context
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName();
-
-        System.out.println("Email: " + email);
-        System.out.println("Authentication: " + authentication.isAuthenticated());
-        System.out.println("Authentication: " + authentication.getAuthorities());
-
-        var user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        System.out.println("User: " + user);
-
-        User user1 = (User) user;
-
-        System.out.println("User1: " + user1);
-
-        ShoppingCart shoppingCart = user1.getShoppingCart();
-
-        System.out.println("Shopping cart: " + shoppingCart);
-
-        System.out.println("Shopping cart products: " + shoppingCart.getProducts());
-
-        // Return the shopping cart information in the response
-        return ResponseEntity.ok().body(shoppingCart.getProducts());
+        return ResponseEntity.ok().body(shoppingCartService.getAllProductsInCart());
     }
 
     @PostMapping("/addProduct")
     public ResponseEntity<List<Product>> addProductToShoppingCart(@RequestBody Map<String, String> payload) {
-        // Retrieve the currently authenticated user's username from the security context
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName();
-
-        System.out.println("posting to shopping cart");
-
-        var user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        System.out.println("User: " + user);
-
-        User user1 = (User) user;
-
-        System.out.println("User1: " + user1);
-
-        ShoppingCart shoppingCart = user1.getShoppingCart();
-
-        Long id = Long.parseLong(payload.get("id"));
-        System.out.println("Product id: " + id);
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Product not found"));
-
-        System.out.println("Product: " + product);
-
-        shoppingCart.addProduct(product);
-        shoppingCartRepository.save(shoppingCart);
-
-        System.out.println("Shopping cart: " + shoppingCart);
-
-        System.out.println("Shopping cart products: " + shoppingCart.getProducts());
-
-        // Return the shopping cart information in the response
-        return ResponseEntity.ok().body(shoppingCart.getProducts());
+        return ResponseEntity.ok().body(shoppingCartService.addProductToCart(payload));
     }
 }
--- Webapi/src/main/java/com/hsleiden/Webapi/service/ShoppingCartService.java
+++ Webapi/src/main/java/com/hsleiden/Webapi/service/ShoppingCartService.java
@@ -1,7 +1,60 @@
 package com.hsleiden.Webapi.service;
 
+import com.hsleiden.Webapi.model.Product;
+import com.hsleiden.Webapi.model.ShoppingCart;
+import com.hsleiden.Webapi.model.User;
+import com.hsleiden.Webapi.repository.ProductRepository;
+import com.hsleiden.Webapi.repository.ShoppingCartRepository;
+import com.hsleiden.Webapi.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ShoppingCartService {
+
+    private final ProductRepository productRepository;
+    private final ShoppingCartRepository shoppingCartRepository;
+    private final UserRepository userRepository;
+
+    public ShoppingCartService(ProductRepository productRepository, ShoppingCartRepository shoppingCartRepository, UserRepository userRepository) {
+        this.productRepository = productRepository;
+        this.shoppingCartRepository = shoppingCartRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public List<Product> addProductToCart(Map<String, String> payload) {
+
+        User user = getAuthenticatedUser();
+        ShoppingCart shoppingCart = user.getShoppingCart();
+
+        Long id = Long.parseLong(payload.get("id"));
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        shoppingCart.addProduct(product);
+        shoppingCartRepository.save(shoppingCart);
+        return shoppingCart.getProducts();
+    }
+
+    public List<Product> getAllProductsInCart() {
+        User user = getAuthenticatedUser();
+        ShoppingCart shoppingCart = user.getShoppingCart();
+        return shoppingCart.getProducts();
+    }
+
+    private User getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+
+        var user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return (User) user;
+    }
 }
