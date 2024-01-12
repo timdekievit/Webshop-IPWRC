83a635c618cb7ad8d3fbcf2c190d9f43321bcaef
Tim <timdekievit@hotmail.com>
2024-01-12 15:54:31 +0100

    re enabled adminguard

--- client-app/src/app/app-routing.module.ts
+++ client-app/src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
   { path: 'confirmation', component: ConfirmationComponent},
   { path: 'management', 
     component: ManagementComponent, 
-    // canActivate: [AdminGuard],
+    canActivate: [AdminGuard],
     children: [
       { path: '', redirectTo: 'orders', pathMatch: 'full' },
       { path: 'orders', component: ManagementOrdersComponent },
